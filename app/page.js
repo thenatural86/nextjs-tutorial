735be2aee91e379aef28757bd61d5c97dbac1871
thenatural86 <philip.smalls86@gmail.com>
2025-01-04 19:16:35 -0500

    adds link

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
+import Link from 'next/link'
+
 const HomePage = () => {
   return (
     <div>
       <h1 className='text-7xl'>HomePage</h1>
+      <Link href='/about' className='text-2xl'>
+        about page
+      </Link>
     </div>
   )
 }
