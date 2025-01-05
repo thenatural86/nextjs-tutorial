52b41ad82266cd0769b40d9b14870c40ac4337d7
thenatural86 <philip.smalls86@gmail.com>
2025-01-05 15:14:32 -0500

    adds local images

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,6 +1,9 @@
 import Link from 'next/link'
-
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
+
+import drinkImg from './drink.jpg'
+import Image from 'next/image'
+console.log(drinkImg)
 
 const fetchDrink = async (id) => {
   const response = await fetch(`${url}${id}`)
@@ -20,6 +23,7 @@ const SingleDrinkPage = async ({ params }) => {
       <Link href='/drinks' className='btn btn-primary mt-8 mb-12'>
         back to drinks
       </Link>
+      <Image src={drinkImg} className='w-48 h-48 rounded' alt='' />
       <h1 className='text-4xl mb-8'>{title}</h1>
     </div>
   )
