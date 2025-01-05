5c6c508031da6f8e60ed637be8ad8204019f8b98
thenatural86 <philip.smalls86@gmail.com>
2025-01-05 15:23:36 -0500

    remote images

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -23,7 +23,14 @@ const SingleDrinkPage = async ({ params }) => {
       <Link href='/drinks' className='btn btn-primary mt-8 mb-12'>
         back to drinks
       </Link>
-      <Image src={drinkImg} className='w-48 h-48 rounded' alt='' />
+      <Image
+        src={imgSrc}
+        width={300}
+        height={300}
+        className='w-48 h-48 rounded shadow-lg mb-4'
+        priority
+        alt=''
+      />
       <h1 className='text-4xl mb-8'>{title}</h1>
     </div>
   )
