4b386e0d37781646c464712d5a569a7a98c1eef7
thenatural86 <philip.smalls86@gmail.com>
2025-01-05 10:33:02 -0500

    adds theme and button

--- app/page.js
+++ app/page.js
@@ -3,7 +3,10 @@ import Link from 'next/link'
 const HomePage = () => {
   return (
     <div>
-      <h1 className='text-7xl'>HomePage</h1>
+      <h1 className='text-5xl mb-8 font-bold'>Next.js tutorial</h1>
+      <Link href='/client' className='btn btn-accent'>
+        get started
+      </Link>
     </div>
   )
 }
