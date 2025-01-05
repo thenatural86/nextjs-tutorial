9c224189001bb2286fbe1b593c9ddf0b78409a97
thenatural86 <philip.smalls86@gmail.com>
2025-01-05 11:10:54 -0500

    async server side fetching

---
+++ app/drinks/loading.js
@@ -0,0 +1,5 @@
+const loading = () => {
+  return <span className='loading'>loading</span>
+}
+
+export default loading
--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,11 +1,19 @@
+import loading from './loading'
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
 
-const DrinksPage = async () => {
+const fetchDrinks = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
   const response = await fetch(url)
   const data = await response.json()
+  return data
+}
+
+const DrinksPage = async () => {
+  const data = await fetchDrinks()
   console.log(data)
   return (
     <div>
+      <loading />
       <h1 className='text-7xl'>DrinksPage</h1>
     </div>
   )
