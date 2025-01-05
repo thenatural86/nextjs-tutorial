0d8782f5f92ae6a7b14995b8a93a931df2d1c0cd
thenatural86 <philip.smalls86@gmail.com>
2025-01-05 10:54:08 -0500

    server component data fetching

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,4 +1,9 @@
-const DrinksPage = () => {
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
+
+const DrinksPage = async () => {
+  const response = await fetch(url)
+  const data = await response.json()
+  console.log(data)
   return (
     <div>
       <h1 className='text-7xl'>DrinksPage</h1>
