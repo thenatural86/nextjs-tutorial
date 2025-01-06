42eb56fc400987f0337966b563e07c76e49ea8db
thenatural86 <philip.smalls86@gmail.com>
2025-01-05 23:48:46 -0500

    render task list from db

--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,7 +1,14 @@
+import DeleteForm from '@/components/DeleteForm'
+import TaskForm from '@/components/TaskForm'
+import TaskList from '@/components/TaskList'
+
 const TasksPage = () => {
   return (
     <div>
       <h1 className='text-7xl'>TasksPage</h1>
+      <TaskForm />
+      <TaskList />
+      <DeleteForm />
     </div>
   )
 }
