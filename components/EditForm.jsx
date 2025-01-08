8399644173d3a680ba6973c82df5d0c5ed581c46
thenatural86 <philip.smalls86@gmail.com>
2025-01-07 22:24:11 -0500

    edit task actions

---
+++ components/EditForm.jsx
@@ -0,0 +1,6 @@
+const EditForm = (task) => {
+  console.log(task)
+  return <div>EditForm</div>
+}
+
+export default EditForm
--- utils/actions.js
+++ utils/actions.js
@@ -27,3 +27,16 @@ export const deleteTask = async (formData) => {
   })
   revalidatePath('/tasks')
 }
+
+export const getTask = async (id) => {
+  return prisma.task.findUnique({
+    where: { id },
+  })
+}
+
+export const editTask = async (formData) => {
+  const id = formData.get('id')
+  await prisma.task.find({
+    where: { id },
+  })
+}
